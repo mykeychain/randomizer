92cd9ca1c95964979964ef940762d048583bc0a2
Mikey <mchang95@gmail.com>
2024-03-31 21:29:53 -0700

    Project Init: Icons are courtesy of nvigneux and ndorfin in their repository https://github.com/nvigneux/Helldivers-2-Stratagems-icons-svg initially starting this project with intention to connect to a relational database, however this may change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [stratagems, setStratagems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchStratagems = async () => {
+      const response = await fetch("http://localhost:3001/stratagems");
+      const data = await response.json();
+      setStratagems(data);
+      setIsLoading(false);
+    };
+
+    fetchStratagems();
+  }, [])
+
+  function renderStratagems( stratagems ) {
+    if (isLoading) {
+      return ([<div> still loading </div>]);
+    }
+    else {
+      const items = [];
+      for (const s of stratagems){
+        items.push(<div key={s.display_name}>{s.display_name}</div>);
+      };
+      return (items);
+    };
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {renderStratagems(stratagems)}
       </header>
     </div>
   );
