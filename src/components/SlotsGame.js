8b9f56ed993204dc2a286b0478714f02550e6be3
mykeychain <mchang95@gmail.com>
2024-04-19 09:34:49 -0700

    [FIX] bug on build from a change that should have been stashed

--- src/components/SlotsGame.js
+++ src/components/SlotsGame.js
@@ -1,17 +1,15 @@
 import React from "react";
 import SlotsButton from "./SlotsButton";
 import SlotsRow from "./SlotsRow";
-import RatingBox from "./RatingBox";
 
 
 function SlotsGame() {
     return (
         <div>
             <SlotsRow />
-            <RatingBox />
             <SlotsButton />
         </div>
-    )
+    );
 };
 
 export default SlotsGame;
