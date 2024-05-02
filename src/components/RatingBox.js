2dcd14ffaf0336456bfe0586893ccc8a4c4163ee
mykeychain <mchang95@gmail.com>
2024-05-01 22:27:40 -0700

    [ADD] basic styling and logic for Rating Box

--- src/components/RatingBox.js
+++ src/components/RatingBox.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "../styles/RatingBox.css";
+import AppContext from "../utils/AppContext";
 
 function RatingBox() {
+    const { appMode } = useContext(AppContext);
+    const [ showBox, setShowBox ] = useState(false);
+
+    useEffect(() => {
+        if (appMode === "resolved") {
+            setTimeout(() => setShowBox(true), 900);
+        } else {
+            setShowBox(false);
+        };
+    }, [appMode]);
+
     return (
         <div className="rating-container">
-            <div className="rating-box">
+            <div className={`rating-box ${showBox ? "visible" : "hidden"} mid`}>
                 <div className="rating-hatch left"/>
                 <div className="rating">
                     CATASTROPHIC
--- src/components/SlotsGame.js
+++ src/components/SlotsGame.js
@@ -1,12 +1,14 @@
 import React from "react";
 import SlotsButton from "./SlotsButton";
 import SlotsRow from "./SlotsRow";
+import RatingBox from "./RatingBox";
 
 
 function SlotsGame() {
     return (
         <div>
             <SlotsRow />
+            <RatingBox />
             <SlotsButton />
         </div>
     );
